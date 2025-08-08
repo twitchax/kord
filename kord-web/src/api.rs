897d701a66ca68c546c35e3159569c526fefec12
Aaron Roney <twitchax@gmail.com>
2025-08-08 23:27:46 +0000

    Remove old web app.

--- kord-web/src/api.rs
+++
@@ -1,8 +0,0 @@
-use axum::{extract::Path, http::StatusCode, response::IntoResponse};
-
-pub async fn hello(Path(name): Path<String>) -> impl IntoResponse {
-    match super::shared::hello(name).await {
-        Ok(greeting) => (StatusCode::OK, greeting),
-        Err(e) => (StatusCode::BAD_REQUEST, e.to_string()),
-    }
-}
--- kord-web/src/main.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(feature = "ssr")]
-fn main() {}
-
-#[cfg(not(feature = "ssr"))]
-pub fn main() {
-    // no client-side main function
-    // unless we want this to work with e.g., Trunk for pure client-side testing
-    // see lib.rs for hydration function instead
-}
